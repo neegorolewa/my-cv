347e8edde8fde9d025d6fd1160913311373d7e36
Eugene <cullofdute19762005@gmail.com>
2024-11-02 03:30:13 +0300

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() 
+{
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className={styles.container}>
+    <h1>Габдрахманов Евгений</h1>
+    <h2> Android-разработчик</h2>
+    <h3>+7(917)716-39-36</h3>
+    <div>
+      <h2>Навыки</h2>
+      <ul>
+        <li>Andriod-разработка(Kotlin)</li>
+        <li>ООП(C++, C#)</li>
+        <li>C#(опыт)</li>
+        <li>SQL(работа с БД)</li>
+        <li>React, JS, TS (опыт)</li>
+      </ul>
     </div>
+    <div>
+      <h2>Опыт работы</h2>
+      <div>
+        <h3>Omega-R, Йошкар-Ола</h3>
+        <p>Android-разработчик</p>
+        <p>июль 2023 - сентябрь 2023</p>
+        <p>Практика и последующая стажировка в компании.
+          Работал на базе Android Studio на Kotlin. Задача: разработать экран логинации пользователя. 
+          Создал локальную базу данных для сохранения логина и пароля пользователя. 
+          Выполнил работу, необходимую для дальнейшей стажировки
+        </p>
+      </div>
+    </div>
+    <div>
+      <h2>Образование</h2>
+      <div>
+        <p>ПГТУ(Волгатех), Йошкар-Ола</p>
+        <p>3 курс, Бакалавр</p>
+        <p>Сентябрь 2022 - н.в.</p>
+        <p>ФИиВТ, Разработчик программных систем</p>
+      </div>
+    </div>
+    <div>
+      <h2>Языки</h2>
+      <p>Русский(идеальный), Английский(A2)</p>
+    </div>
+  </div>
   );
 }
